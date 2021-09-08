bf6ccb7a30d712a8d818c0c13cb050cb1bd1014b
iroviram <ram.villarr@gmail.com>
2021-09-08 17:59:51 -0500

    proyecto

--- 4_KataJSAvanzado/Proyecto_3_Pokedex/index.js
+++ 4_KataJSAvanzado/Proyecto_3_Pokedex/index.js
@@ -1,32 +1,24 @@
-//* URI - https://pokeapi.co/api/v2/pokemon/
-
 const URI = 'https://pokeapi.co/api/v2/pokemon/';
 
 function consultarPokemon(id) {
-    fetch(URI+id).then(function(response){
-        if(response.ok) {
-            response.json().then(function (pokemon) {
-                pokemonImage(pokemon);
-            });
-        }
-    })
-    .catch(function(error) {
-        console.log('Hubo un problema con la petición Fetch:' + error.message);
-    });
+    fetch(URI+id)
+        .then(response => response.json())
+        .then(pokemon => pokemonImage(pokemon));
 }
 
 function pokemonImage(pokemon){
     let pokemonOrderNum = pokemon.order;
-    /* let pokemonSpriteFront = pokemon['sprites']['front_default']; */
+    // let pokemonSpriteFront = pokemon['sprites']['front_default'];
     let pokemonSpriteFront = pokemon['sprites']['other']['official-artwork']['front_default'];
-    let pokemonSpritFrontString = pokemonSpriteFront.replace(/"/g,'')
-    return document.getElementById(`pokemon-${pokemonOrderNum}`).style.backgroundImage = `url(${pokemonSpritFrontString})`;
+    let pokemonSpriteFrontString = pokemonSpriteFront.replace(/"/g,'')
+    return document.getElementById(`pokemon-${pokemonOrderNum}`).style.backgroundImage = `url(${pokemonSpriteFrontString})`;
 }
 
-var numPokemonBoxes = document.querySelectorAll('li.pokemon-box').length
-console.log(numPokemonBoxes)
-for (let i = 1; i <= numPokemonBoxes; i++) {
-    consultarPokemon(i);
+function pokemonNumberSearch(){
+    var numPokemonBoxes = document.querySelectorAll('li.pokemon-box').length
+    for (let i = 1; i <= numPokemonBoxes; i++) {
+        consultarPokemon(i)
+    }
 }
 
-window.onload = consultarPokemon;
+window.onload = pokemonNumberSearch;
---
+++ 4_KataJSAvanzado/Proyecto_3_Pokedex/index2.js
@@ -0,0 +1,61 @@
+//? First Form
+
+/* let promises = [];
+var numPokemonBoxes = document.querySelectorAll('li.pokemon-box').length
+for (let i = 1; i <= numPokemonBoxes; i++) {
+  promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(response => response.json()));
+}
+
+function fetchPokemon(){
+  Promise.all(promises)
+  .then(response => {
+    pokemonImage(response);
+  });
+}
+
+function pokemonImage(pokemon){
+  var numPokemonBoxes = document.querySelectorAll('li.pokemon-box').length
+  for (let i = 0; i <= numPokemonBoxes; i++){
+    let pokemonOrderNum = pokemon[i]['order'];
+    // let pokemonSpriteFront = pokemon['sprites']['front_default'];
+    let pokemonSpriteFront = pokemon[i]['sprites']['other']['official-artwork']['front_default'];
+    let pokemonSpritFrontString = pokemonSpriteFront.replace(/"/g,'')
+    document.getElementById(`pokemon-${pokemonOrderNum}`).style.backgroundImage = `url(${pokemonSpritFrontString})`;
+  }
+}
+
+fetchPokemon();
+window.onload = fetchPokemon; */
+
+//? Second Form
+
+const URI = 'https://pokeapi.co/api/v2/pokemon/';
+
+/* function consultarPokemon(id) {
+    fetch(URI+id)
+        .then(response => response.json())
+        .then(pokemon => pokemonImage(pokemon));
+} */
+
+function getPokemonByName(name){
+    return new Promise((resolve,reject)=>{
+        fetch(URI+name)
+              .then(response => response.json())
+    }
+)};
+
+async function getPokemonStatsByNameDos(pokemonName){
+  // si funcion cae en RESOLVE
+  try{
+      const pokemonData = await getPokemonByName(pokemonName);
+      console.log(pokemonData);
+      const stats = pokemonData.objeto.stats.map((objeto)=>[objeto.stat.name, objeto.base_stat]);
+      console.log(`Las estadisticas de ${pokemonName}`,stats);
+  }catch(err){
+      console.log(err);
+  }
+}
+
+getPokemonStatsByNameDos("pikachu");
+window.onload = getPokemonStatsByNameDos;
+
