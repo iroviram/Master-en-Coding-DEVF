35e283e7cddf98a3f7114dda545ec63b6b25ba3a
iroviram <ram.villarr@gmail.com>
2021-10-20 21:32:56 -0500

    Estructura y Netflify

---
+++ 05.React/11.react-crypto/src/routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Home from '../views/Home/Home';
+import News from '../views/News/News';
+import Contact from '../views/Contact/Contact';
+import Error404 from '../views/Error404/Error404';
+import Navbar from '../components/Navbar/Navbar';
+
+const Routes = () => {
+
+    return (
+        <div>
+            <Router>
+            <Navbar/>
+                <Switch>
+                    <Route exact path="/" component ={Home} />
+                    <Route exact path="/news" component ={News} />
+                    <Route exact path="/contact" component ={Contact} />
+                    <Route path="*" component ={Error404} />
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+export default Routes;
---
+++ 05.React/11.react-crypto/src/views/Home/Home.js
@@ -0,0 +1,77 @@
+import React,{ useState, useEffect } from 'react'
+import useAxios from '../../hooks/useAxios';
+import Coin from '../../components/Coin/Coin';
+
+const Home = () => {
+    const [coins,setCoins] = useState([]);
+    const [search,setSearch] = useState('');
+
+    const { response, loading, error } = useAxios({
+        method: 'GET',
+        url: 'coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+    });
+
+    const handleChange = e => {
+        setSearch(e.target.value)
+    }
+
+    useEffect(() => {
+        if(response !== null){
+            setCoins(response);
+            console.log(coins)
+        }
+    },[response]);
+
+    const filteredCoins = coins && coins.filter(coin => 
+            coin.name.toLowerCase().includes(search.toLowerCase())
+        );
+
+    return (
+        <>
+            <h1>Home</h1>
+            
+            <div className='coin-app'>
+                <div className='coin-search'>
+                    <h1 className='coin-text'>Search a currency</h1>
+                    <form>
+                        <input
+                            className='coin-input'
+                            type='text'
+                            onChange={handleChange}
+                            placeholder='Search'
+                        />
+                    </form>
+                </div>
+
+                {error && (
+                    <div>
+                        <p>{error.message}</p>
+                    </div>
+                )}
+
+                {coins && 
+                    <div>
+                        {filteredCoins.map(coin => {
+                            return(
+                                <Coin
+                                    key={coin.id}
+                                    name={coin.name}
+                                    price={coin.current_price}
+                                    symbol={coin.symbol}
+                                    marketcap={coin.market_cap}
+                                    volume={coin.total_volume}
+                                    image={coin.image}
+                                    priceChange={coin.price_change_percentage_24h}
+                                />
+                            )
+                        })
+
+                        }
+                    </div>
+                }
+            </div>
+        </>
+    )
+}
+
+export default Home
